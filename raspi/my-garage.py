7ed8740cf47784f95d3f8cfa44c714c4a04743e7
Stephen Papierski <stephenpapierski@gmail.com>
2020-01-20 05:44:03 +0000

    Adding ability to handle POST requests from the raspberry pi side

--- raspi/my-garage.py
+++ raspi/my-garage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import gpiozero
+from flask import Flask, request
 
 class distanceSensor(gpiozero.MCP3001):
     def __init__(self, max_voltage=3.3, **spi_args):
@@ -15,19 +16,31 @@ class distanceSensor(gpiozero.MCP3001):
     def open(self):
         return self.value <= 100
 
-
-
 # Main Program
-distanceS = distanceSensor(max_voltage = 3.3,
-                             clock_pin = 11,
-                             mosi_pin = 10,
-                             miso_pin = 9,
-                             select_pin = 7)
+app = Flask(__name__)
+@app.route('/myGarage/open/', methods=['POST'])
+def open():
+    print(request.form)
+    return 'Opening Garage Door...'
 
+@app.route('/myGarage/close/', methods=['POST'])
+def close():
+    print(request.form)
+    return 'Closing Garage Door...'
 
-while (True):
-    pass
-    print("raw: \t" + str(distanceS.raw_value))
-    print("CM: \t" + str(distanceS.distanceCM))
-    print("Closed: \t" + str(distanceS.closed))
-    print("Open: \t" + str(distanceS.open))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+#distanceS = distanceSensor(max_voltage = 3.3,
+#                             clock_pin = 11,
+#                             mosi_pin = 10,
+#                             miso_pin = 9,
+#                             select_pin = 7)
+#
+#
+#while (True):
+    #pass
+#    print("raw: \t" + str(distanceS.raw_value))
+#    print("CM: \t" + str(distanceS.distanceCM))
+#    print("Closed: \t" + str(distanceS.closed))
+#    print("Open: \t" + str(distanceS.open))
+
