2c783f8d3a448f97ba5fa434908ae9dda051a59e
Stephen Papierski <stephenpapierski@gmail.com>
2020-02-01 05:57:17 +0000

    Added stopped status, relay logic based on status, and a timeout value.

--- raspi/pi-garage.py
+++ raspi/pi-garage.py
@@ -42,63 +42,115 @@ class garageDoor():
         self._wasFullyClosed = None
         self._wasFullyOpen = None
         self._status = None
-        self._updateStatus()
+        self._stopNew = False
+        self._startTime = None
+        self._statusFile = "piGarageStatus"
+        # Run Configure with default params
+        #TODO default to 15 seconds
+        self.updateSettings(transitionTime=30, actuateDuration = 1000)
+        self._updateGPIOStatus()
 
-    # Check sensors and update global variables
-    def _updateStatus(self):
-        self._closed = GPIO.input(self._closePin)
-        self._open = GPIO.input(self._openPin)
+    ##########################################################################
+    # Public functions
+    ##########################################################################
 
-    # Actuate the relay to open/close door
-    def _actuateDoor(self):
-        GPIO.output(self._relayPin, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(self._relayPin, GPIO.LOW)
+    # Update the configurable parameters of the sysem
+    def updateSettings(self, transitionTime, actuateDuration):
+        #self._urlPath = urlPath
+        self._transitionTime = transitionTime
+        self._actuateDuration = actuateDuration
 
-    # Return true if door is in FULLY closed position, false if not
-    def isFullyClosed(self):
-        self._updateStatus()
-        return self._closed
-
-    # Return true if door is in FULLY open position, false if not
-    def isFullyOpen(self):
-        self._updateStatus()
-        return self._open
-
+    # Try to close the garage door
     def closeDoor(self):
-        if not self.isFullyClosed():
-            self._actuateDoor()
-            print("Close Door")
-        else:
-            print("Door already closed")
+        self._readStatus()
+        print('Status: '+self._status)
 
+        # if open -> actuate 1x
+        if (self._status == "open"):
+            self._actuateRelay()
+            print ("Door closing")
+        # if closed -> do nothing
+        elif (self._status == "closed"):
+            print ("Door already closed")
+        # if opening -> actuate 2x
+        elif (self._status == "opening"):
+            self._doubleActuateDoor()
+            print ("Door stopping then closing")
+        # if closing -> do nothing
+        elif (self._status == "closing"):
+            print ("Door already closing")
+        # if stopped -> actuate 1x
+        elif (self._status == "stopped"):
+            self._actuateRelay()
+            print("Door closing")
+        else:
+            print ("Didn't do anything")
+
+    # Try to open the garage door
     def openDoor(self):
-        if not self.isFullyOpen():
-            self._actuateDoor()
-            print("Open Door")
+        self._readStatus()
+        print('Status: '+self._status)
+
+        # if open -> do nothing
+        if (self._status == "open"):
+            print ("Door already open")
+        # if closed -> actuate 1x
+        elif (self._status == "closed"):
+            self._actuateRelay()
+            print ("Door opening")
+        # if opening -> do nothing
+        elif (self._status == "opening"):
+            print ("Door already opening")
+        # if closing -> actuate 1x
+        elif (self._status == "closing"):
+            self._actuateRelay()
+            print ("Door opening")
+        # if stopped -> actuate 2x
+        elif (self._status == "stopped"):
+            self._doubleActuateDoor()
+            print ("Door closing then opening")
         else:
-            print("Door already open")
+            print ("Didn't do anything")
 
-
-    def getStatus(self):
-        isFullyClosed = self.isFullyClosed()
-        isFullyOpen = self.isFullyOpen()
+    # This should be called frequenty in a main loop to keep everything up to date
+    # @returns  (newStatus, status)
+    #           newStatus   boolean     is this status new?
+    #           status      string      current status value
+    def checkNewStatus(self):
+        isFullyClosed = self._isFullyClosed()
+        isFullyOpen = self._isFullyOpen()
         newStatus = None
 
+        # Check if the time has elapsed (door is stopped partially open)
+        if (self._startTime):
+            elapsedTime = time.time()-self._startTime
+            if (elapsedTime > self._transitionTime):
+                self._startTime = None
+                newStatus = "stopped"
+
+        # Check for new absolute states
         if (isFullyClosed != self._wasFullyClosed):
             # Report door is now fully closed
             if (isFullyClosed):
                 newStatus = "closed"
+                # Reset timer when we reach fully closed
+                self._startTime = None
             # Report door is no longer closed
             else:
                 newStatus = "opening"
+                # Start timer
+                self._startTime = time.time()
         elif (isFullyOpen != self._wasFullyOpen):
             # Report door is now fully open
             if(isFullyOpen):
                 newStatus = "open"
+                # Reset timer when we reach fully open
+                self._startTime = None
             # Report door is no longer fully open
             else:
                 newStatus = "closing"
+                # Start timer
+                self._startTime = time.time()
         if (isFullyOpen and isFullyClosed):
             newStatus = "unknown"
 
@@ -107,21 +159,69 @@ class garageDoor():
 
         if (newStatus):
             self._status = newStatus
+            self._writeStatus()
             return (True, newStatus)
         else:
+            # Report existing status
             return (False, self._status)
+
+    ##########################################################################
+    # Internal functions
+    ##########################################################################
+
+    # Check sensors and update global variables
+    def _updateGPIOStatus(self):
+        self._closed = GPIO.input(self._closePin)
+        self._open = GPIO.input(self._openPin)
+
+    # Return true if door is in FULLY closed position, false if not
+    def _isFullyClosed(self):
+        self._updateGPIOStatus()
+        return self._closed
+
+    # Return true if door is in FULLY open position, false if not
+    def _isFullyOpen(self):
+        self._updateGPIOStatus()
+        return self._open
+
+    # Actuate the relay to open/close door
+    def _actuateRelay(self):
+        print("Actuating Relay...")
+        GPIO.output(self._relayPin, GPIO.HIGH)
+        time.sleep(float(self._actuateDuration)/1000)
+        GPIO.output(self._relayPin, GPIO.LOW)
+
+    # Double actuate the relay to open/close door
+    def _doubleActuateDoor(self):
+        self._actuateRelay()
+        time.sleep(self._actuateDuration/1000)
+        self._actuateRelay()
+
+    # Write the status to the statusFile. This should be performed by the main 
+    # thread so that the background Flask thread can read the current status 
+    # from the file.
+    def _writeStatus(self):
+        f = open(self._statusFile, "w")
+        f.write(self._status)
+        pass
+    # Read the status from the statusFile. This should be performed by the 
+    # background Flask thread to get the current status of the main thread.
+    def _readStatus(self):
+        f = open(self._statusFile, "r")
+        self._status = f.read()
+
 
 # Main Program
 # Flask app for receiving POST Requests
 app = Flask(__name__)
 @app.route('/PiGarage/open/', methods=['POST'])
-def open():
+def open_command():
     #print(request.form)
     garage.openDoor()
     return 'Opening Garage Door...'
 
 @app.route('/PiGarage/close/', methods=['POST'])
-def close():
+def close_command():
     #print(request.form)
     garage.closeDoor()
     return 'Closing Garage Door...'
@@ -129,7 +229,7 @@ def close():
 # Main loop to run in the background
 def garage_loop():
     while (True):
-        (newStatus, status) = garage.getStatus()
+        (newStatus, status) = garage.checkNewStatus()
 
         # Hubitat receives unsolicited http post requests on port 39501
         # and routes the message to the parse() method of the device 
